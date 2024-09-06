039c7c0764b792a0fec11ee081093751bd19480e
daniel-salin <daniel.salin@gmail.com>
2024-09-06 16:32:09 +0200

    complete assignment 4

--- Lesson_4/uppgfit_4/app.py
+++ Lesson_4/uppgfit_4/app.py
@@ -1,3 +1,4 @@
+import user_modules
 """Skapa ett program uppdelat i tre filer:"""
 
 """En modul för att lägga till användare (med namn
@@ -12,5 +13,23 @@ returnera deras information."""
 (lägga till, söka eller visa alla användare)."""
 
 def user_management():
+  user_list = []
+  user_choice = int(input("1) Add new user\n2) Find user by name\n3) List all users\n4) Quit program\n"))
+  while user_choice != 4:
+    if user_choice == 1:
+      user_modules.add_user(user_list)
+      print("User addded")
+      user_choice = int(input("1) Add new user\n2) Find user by name\n3) List all users\n4) Quit program\n"))
+    elif user_choice == 2:
+      user_name = input("Enter a name to query: ")
+      user_modules.search_user(user_list, user_name)
+      user_choice = int(input("1) Add new user\n2) Find user by name\n3) List all users\n4) Quit program\n"))
+    elif user_choice == 3:
+      user_modules.print_users(user_list)
+      user_choice = int(input("1) Add new user\n2) Find user by name\n3) List all users\n4) Quit program\n"))
+    elif user_choice == 4:
+      print("Closing program")
+      break
   return None
 
+user_management()
