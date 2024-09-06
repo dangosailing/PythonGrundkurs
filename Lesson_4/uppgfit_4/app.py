1174ab9745eb45c44ea137153e315eed019119eb
daniel-salin <daniel.salin@gmail.com>
2024-09-06 15:33:00 +0200

    Add app file with basic instructions for user management

---
+++ Lesson_4/uppgfit_4/app.py
@@ -0,0 +1,16 @@
+"""Skapa ett program uppdelat i tre filer:"""
+
+"""En modul för att lägga till användare (med namn
+och ålder) till en lista."""
+
+"""En modul för att söka efter användare i listan och
+returnera deras information."""
+
+"""En modul för att skriva ut alla användare."""
+
+"""Huvudprogrammet ska låta användaren välja vilken operation de vill utföra
+(lägga till, söka eller visa alla användare)."""
+
+def user_management():
+  return None
+
