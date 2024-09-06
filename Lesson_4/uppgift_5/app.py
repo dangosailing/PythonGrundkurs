f35354f909ac25342d899d2e4a5a25f2bff905d7
daniel-salin <daniel.salin@gmail.com>
2024-09-06 16:48:17 +0200

    add assignment 5 app file w instructions

---
+++ Lesson_4/uppgift_5/app.py
@@ -0,0 +1,8 @@
+"""Dela upp programmet i moduler:
+En modul för att lägga till nya uppgifter.
+En modul för att markera uppgifter som klara.
+En modul för att ta bort uppgifter.
+En modul för att visa uppgifter
+Använd en lista för att lagra uppgifterna och en while-loop för att hålla
+programmet interaktivt tills användaren väljer att avsluta."""
+
