bd5844d4bcbba656a6132a7774c9740b5e48ff24
daniel-salin <daniel.salin@gmail.com>
2024-09-06 15:46:14 +0200

    add search_User to user_modules

--- Lesson_4/uppgfit_4/user_modules.py
+++ Lesson_4/uppgfit_4/user_modules.py
@@ -4,3 +4,12 @@ def add_User(user_list:list):
   user = {"name":user_name, "age":user_age} 
   user_list.append(user)
   return user_list
+
+def search_User(user_list:list, user_name):
+  found_user_list = []
+  if len(user_list) > 0:
+    for user in user_list:
+      if user["name"] == user_name:
+        print(f"name: {user["name"]} age: {user["age"]}")
+        found_user_list.append(user)
+    return found_user_list
