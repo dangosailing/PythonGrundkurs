b3aa14d68c1c6fb109da4aeb638ff43f64ed8a88
daniel-salin <daniel.salin@gmail.com>
2024-09-06 15:47:07 +0200

    lower case user_modules

--- Lesson_4/uppgfit_4/user_modules.py
+++ Lesson_4/uppgfit_4/user_modules.py
@@ -1,11 +1,11 @@
-def add_User(user_list:list):
+def add_user(user_list:list):
   user_name = input("Enter your name")
   user_age = input("Enter your age")
   user = {"name":user_name, "age":user_age} 
   user_list.append(user)
   return user_list
 
-def search_User(user_list:list, user_name):
+def search_user(user_list:list, user_name):
   found_user_list = []
   if len(user_list) > 0:
     for user in user_list:
@@ -13,3 +13,4 @@ def search_User(user_list:list, user_name):
         print(f"name: {user["name"]} age: {user["age"]}")
         found_user_list.append(user)
     return found_user_list
+  
