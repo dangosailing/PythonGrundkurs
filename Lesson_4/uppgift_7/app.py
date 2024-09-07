7080ee04f7ab811f014eae9e74f5a2c7f3c0a469
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 09:42:41 +0200

    complete basic functionality for assignment 7

--- Lesson_4/uppgift_7/app.py
+++ Lesson_4/uppgift_7/app.py
@@ -1,3 +1,4 @@
+import recepie_modules
 """
 7. Receptbok med modulär programmering
 Skapa en modul som hanterar att lägga till recept (titel, ingredienser,
@@ -7,3 +8,23 @@ En modul som listar alla sparade recept.
 Huvudprogrammet ska fråga användaren vilken operation de vill utföra (lägga
 till, söka eller lista recept).
 """
+
+def recepie_book():
+    user_choice = int(input("1) Create new recepie\n2) Search recepie\n3) List recepies\n4) Quit program\n"))
+    recepie_list =[]
+    while user_choice != 4:
+        if user_choice == 1:
+            recepie_list = recepie_modules.add_recepie(recepie_list)
+            print("Recepie added")
+            user_choice = int(input("1) Create new recepie\n2) Search recepie\n3) List recepies\n4) Quit program\n"))
+        elif user_choice == 2:
+            recepie_modules.search_recepie(recepie_list)
+            user_choice = int(input("1) Create new recepie\n2) Search recepie\n3) List recepies\n4) Quit program\n"))
+        elif user_choice == 3:
+            recepie_modules.list_recepies(recepie_list)
+            user_choice = int(input("1) Create new recepie\n2) Search recepie\n3) List recepies\n4) Quit program\n"))
+        elif user_choice == 4:
+            print("Closing program")
+            break
+
+recepie_book()
