de8cb6dbb4fd3216fee1829cbbb05369202833f0
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 09:10:42 +0200

    addRecepie -> add_recepie

--- Lesson_4/uppgift_7/recepie_modules.py
+++ Lesson_4/uppgift_7/recepie_modules.py
@@ -1,4 +1,4 @@
-def addRecepie():
+def add_recepie():
     title = input("Enter the name of the dish:\n")
     ingredients_str = input("List the ingredients in a comma seperated text string:\n")
     ingredients = ingredients_str.split("")
