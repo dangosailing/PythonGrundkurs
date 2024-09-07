da307025b49d2f57fc3acba0405fe85ee8c56121
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 11:00:35 +0200

    add return_books to library_modules

--- Lesson_4/uppgift_9/library_modules.py
+++ Lesson_4/uppgift_9/library_modules.py
@@ -18,3 +18,17 @@ def lend_book(book_list:list):
             if will_lend.lower() == "y":
                 entry["available"] = False
     return book_list
+
+def return_book(book_list:list):
+    print("Boks to be returned: ")
+    print("---------------------")
+    for entry in book_list:
+        if not entry["available"]:
+            print(f"Title: {entry["title"]}, {entry["author"]}")
+    title_query = input("Enter the title of the book you wish to return: ")
+    for entry in book_list:
+        if entry["title"] == title_query and not entry["available"]:
+            will_return = input(f"Do you wish to return {entry["title"]} by {entry["author"]}? (Y/N): ")
+            if will_return.lower() == "y":
+                entry["available"] = True
+    return book_list
