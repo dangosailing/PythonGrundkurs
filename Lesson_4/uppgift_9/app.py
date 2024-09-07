dfdb4e8bdf2fe81ff4980fb9296580c09d68a985
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 11:01:01 +0200

    complete basic functionality for assignment 9

--- Lesson_4/uppgift_9/app.py
+++ Lesson_4/uppgift_9/app.py
@@ -1,3 +1,4 @@
+import library_modules
 """
 9. Bibliotekssystem med flera filer
 Skapa ett enkelt bibliotekssystem som är uppdelat i flera moduler:
@@ -10,3 +11,26 @@ statusen för dessa.
 Använd en huvudmodul som låter användaren hantera biblioteket.
 """
 
+def library_manager():
+    book_list = []
+    user_choice = int(input("1) Add new book\n2) Lend book\n3) Return book\n4) Quit program\n"))
+    while user_choice != 4:
+        print("List out books in system")
+        print(book_list)
+        if user_choice == 1:
+            library_modules.add_book(book_list)
+            print("Book added")
+            user_choice = int(input("1) Add new book\n2) Lend book\n3) Return book\n4) Quit program\n"))
+        elif user_choice == 2:
+            library_modules.lend_book(book_list)
+            print("Book lended")
+            user_choice = int(input("1) Add new book\n2) Lend book\n3) Return book\n4) Quit program\n"))
+        elif user_choice == 3:
+            library_modules.return_book(book_list)
+            print("Book lended")
+            user_choice = int(input("1) Add new book\n2) Lend book\n3) Return book\n4) Quit program\n"))
+        elif user_choice == 4:
+            print("Closing program")
+            break
+
+library_manager()
