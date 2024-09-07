0d4d8d1cfc4c3f97289dd0ca953e6cbc08814cfd
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 10:14:26 +0200

    add search_contacts and strip user input to contact_modules

--- Lesson_4/uppgift_8/contact_modules.py
+++ Lesson_4/uppgift_8/contact_modules.py
@@ -1,6 +1,6 @@
 def add_contact(contact_list:list):
-    name = input("Enter a contact name: ")
-    tel_number = input("Enter their phone number: ")
+    name = input("Enter a contact name: ").strip()
+    tel_number = input("Enter their phone number: ").strip()
     new_contact = {"name": name, "telephone_number": tel_number}
     contact_list.append(new_contact)
     return contact_list
@@ -8,4 +8,16 @@ def add_contact(contact_list:list):
 def list_contacts(contact_list:list):
     for entry in contact_list:
         print(f"name: {entry["name"]}, phone number:{entry["telephone_number"]}")
-        
+
+def search_contacts(contact_list:list):
+    name_query = input("Search for a name in the contact book: ").strip()
+    for entry in contact_list:
+        if entry["name"] == name_query:
+            print(f"name: {entry["name"]}, phone number:{entry["telephone_number"]}")
+        else:
+            print("No contact with that name was found")
+
+contact_test=[]
+contact_test = add_contact(contact_test)
+list_contacts(contact_test)
+search_contacts(contact_test)
