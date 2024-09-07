d9cf29ff3c14bf6b9cd07b6db5a095e04025662b
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 08:55:49 +0200

    add app file for assignment 7 with basic instructions

---
+++ Lesson_4/uppgift_7/app.py
@@ -0,0 +1,9 @@
+"""
+7. Receptbok med modulär programmering
+Skapa en modul som hanterar att lägga till recept (titel, ingredienser,
+instruktioner).
+En modul som söker efter ett recept baserat på titel.
+En modul som listar alla sparade recept.
+Huvudprogrammet ska fråga användaren vilken operation de vill utföra (lägga
+till, söka eller lista recept).
+"""
