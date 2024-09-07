1dca847e885d9f4b487cb35fa7bd9de1d003451d
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 09:09:43 +0200

    Add receipie_modules with addRecepie

---
+++ Lesson_4/uppgift_7/recepie_modules.py
@@ -0,0 +1,7 @@
+def addRecepie():
+    title = input("Enter the name of the dish:\n")
+    ingredients_str = input("List the ingredients in a comma seperated text string:\n")
+    ingredients = ingredients_str.split("")
+    instructions = input("Describe how to prepare the dish:\n")
+    recepie = {"title": title, "ingredients":ingredients, "instructions":instructions}
+    return recepie
