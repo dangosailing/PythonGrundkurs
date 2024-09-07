15efb0e803c50387806488dfeed5a926711ec192
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 09:43:55 +0200

    add app file for assignment 8 with instructions

---
+++ Lesson_4/uppgift_8/app.py
@@ -0,0 +1,11 @@
+"""
+8. Kontaktbok med filhantering
+Dela upp i moduler:
+En modul för att lägga till nya kontakter (namn
+och telefonnummer) och spara dem i en fil.
+En modul för att läsa och visa alla kontakter från
+filen.
+En modul för att söka efter en specifik kontakt i
+filen.
+Huvudprogrammet ska låta användaren välja vilken operation de vill utföra
+"""
