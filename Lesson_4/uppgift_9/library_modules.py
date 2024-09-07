6e293bfc7d139c5d709155d11c790a19d9175ea9
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 10:46:11 +0200

    add lend_book to library_modules

--- Lesson_4/uppgift_9/library_modules.py
+++ Lesson_4/uppgift_9/library_modules.py
@@ -4,3 +4,17 @@ def add_book(book_list:list):
     book = {"title": book_title, "author": book_author, "available": True}
     book_list.append(book)
     return book_list
+
+def lend_book(book_list:list):
+    print("Available books: ")
+    print("---------------------")
+    for entry in book_list:
+        if entry["available"]:
+            print(f"Title: {entry["title"]}, {entry["author"]}")
+    title_query = input("Enter the title of the book you wish to borrow: ")
+    for entry in book_list:
+        if entry["title"] == title_query and entry["available"]:
+            will_lend = input(f"Do you wish to lend out {entry["title"]} by {entry["author"]}? (Y/N): ")
+            if will_lend.lower() == "y":
+                entry["available"] = False
+    return book_list
