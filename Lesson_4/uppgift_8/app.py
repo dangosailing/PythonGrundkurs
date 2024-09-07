9c4cedd5019fe6ec48a9ad8b0b36c16b34b3015e
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 10:24:08 +0200

    complete basic functionality for assignmnet 8

--- Lesson_4/uppgift_8/app.py
+++ Lesson_4/uppgift_8/app.py
@@ -1,3 +1,5 @@
+import contact_modules
+
 """
 8. Kontaktbok med filhantering
 Dela upp i moduler:
@@ -9,3 +11,26 @@ En modul för att söka efter en specifik kontakt i
 filen.
 Huvudprogrammet ska låta användaren välja vilken operation de vill utföra
 """
+
+def contact_book():
+    contact_list = []
+    user_choice = int(input("1) Create new contact\n2) Search contacts by name\n3) List all contacts\n4) Quit program\n"))
+    while user_choice != 4:
+        if user_choice == 1:
+            contact_modules.add_contact(contact_list)
+            print("Contact added")
+            user_choice = int(input("1) Create new contact\n2) Search contacts by name\n3) List all contacts\n4) Quit program\n"))
+            
+        elif user_choice == 2:
+            contact_modules.search_contacts(contact_list)
+            user_choice = int(input("1) Create new contact\n2) Search contacts by name\n3) List all contacts\n4) Quit program\n"))
+            
+        elif user_choice == 3:
+            contact_modules.list_contacts(contact_list)
+            user_choice = int(input("1) Create new contact\n2) Search contacts by name\n3) List all contacts\n4) Quit program\n"))
+            
+        elif user_choice == 4:
+            print("Closing program")
+            break
+
+contact_book()
