945b83c9177faf0e29c9bcae73ad9927ab931033
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 10:29:05 +0200

    add app file with instructions for assignment 9

---
+++ Lesson_4/uppgift_9/app.py
@@ -0,0 +1,12 @@
+"""
+9. Bibliotekssystem med flera filer
+Skapa ett enkelt bibliotekssystem som är uppdelat i flera moduler:
+En modul för att lägga till böcker (titel och
+författare) i en lista.
+En modul för att låna ut böcker och uppdatera
+listan över tillgängliga böcker.
+En modul för att returnera böcker och uppdatera
+statusen för dessa.
+Använd en huvudmodul som låter användaren hantera biblioteket.
+"""
+
