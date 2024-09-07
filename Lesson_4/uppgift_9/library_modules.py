0f5ac66b43911e70402fd8c8348fd7d3a7cc28ce
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 10:31:57 +0200

    add "available" key value pair to book entry in library_modules

--- Lesson_4/uppgift_9/library_modules.py
+++ Lesson_4/uppgift_9/library_modules.py
@@ -1,6 +1,6 @@
 def add_book(book_list:list):
     book_title = input("Enter book title: ")
     book_author = input("Enter book author: ")
-    book = {"title": book_title, "author": book_author}
+    book = {"title": book_title, "author": book_author, "available": True}
     book_list.append(book)
     return book_list
