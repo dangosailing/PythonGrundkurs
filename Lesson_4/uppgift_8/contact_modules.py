c75da87e7b55f3e81848bdfb1ad1e49ecb13f78e
Daniel Salin <daniel.salin@gmail.com>
2024-09-07 10:09:51 +0200

    add list_contacts to contact_moduels

--- Lesson_4/uppgift_8/contact_modules.py
+++ Lesson_4/uppgift_8/contact_modules.py
@@ -4,3 +4,8 @@ def add_contact(contact_list:list):
     new_contact = {"name": name, "telephone_number": tel_number}
     contact_list.append(new_contact)
     return contact_list
+
+def list_contacts(contact_list:list):
+    for entry in contact_list:
+        print(f"name: {entry["name"]}, phone number:{entry["telephone_number"]}")
+        
